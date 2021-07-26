f111e2de6f6c31dce64fe8ca47de2871c0b20d40
a1cd <71281043+a1cd@users.noreply.github.com>
2021-07-26 10:53:10 -0400

    Initial Commit

---
+++ Game/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Game
+//
+//  Created by Everett Wilber on 7/26/21.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
